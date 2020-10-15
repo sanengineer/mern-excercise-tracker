d07cfa171a53f058b87a2b70c331d8a3d4585603
Ikhsan sugiarto <48153104+sanengineer@users.noreply.github.com>
2020-10-15 22:20:06 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "../bootstrap/css/bootstrap.min.css";
+import "./bootstrap/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
